4cfbab2ece90829ec355e3bed940c7d0d3d0ac33
Sohaila Ashraf Ahmad <87656621+sohailaashraf14@users.noreply.github.com>
2026-09-02 23:00:46 +0300

    Redirect to Signin.html after successful sign-out

--- JS/signout.js
+++ JS/signout.js
@@ -2,7 +2,7 @@ function Signout() {
     if (localStorage.getItem("active-user")) {
         localStorage.removeItem("active-user");
         alert("Sign-out successful!");
-        location.href = "index.html";
+        location.href = "/Signin.html";
     } else {
         alert("No user is currently signed in.");
     }
